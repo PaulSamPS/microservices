a5eaa96b5c3ec209e64355dcea46ef33601864c8
Павел Самойленко <spv@vt2b.ru>
2024-07-05 11:55:32 +0500

    upd: add imports

--- libs/post/src/aplication-services/queries/index.ts
+++ libs/post/src/aplication-services/queries/index.ts
@@ -1,7 +1,9 @@
 import { IQueryHandler } from '@nestjs/cqrs';
 import { Type } from '@nestjs/common';
 import { GetPostQueryHandler } from './get-post/get-post.query.handler';
+import { GetPostsQueryHandler } from './get-posts/get-post.query.handler';
 
 export const POST_QUERIES_HANDLERS: Type<IQueryHandler>[] = [
   GetPostQueryHandler,
+  GetPostsQueryHandler,
 ];
